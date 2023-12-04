02778ee5eab1da8a4f98c2b41218cfcbddb9ee9c
Rangga Arya Putra <107985465+RanggaBot@users.noreply.github.com>
2023-12-04 07:47:14 +0700

    quiz

---
+++ Assets/Scripts/PengolahSoal.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PengolahSoal : MonoBehaviour
+{
+     public TextAsset assetSoal;
+     private string[] soal;
+
+     private string [,] soalBag;
+
+     int indexSoal;
+     int maxSoal;
+     bool ambilsoal;
+     char kuncij;
+
+     bool[] soalSelesai;
+
+     //komponen UI
+     public Text txtSoal, txtOpsiA,txtOpsiB,txtOpsiC,txtOpsiD;
+
+      bool isHasil;
+      private float durasi;
+      public float durasiPenilaian;
+
+      int jwbBenar, jwbSalah;
+      float nilai;
+
+      public GameObject panel;
+      public GameObject imgPenilaian, imgHasil;
+      public Text txtHasil;
+
+    //Start is called before the first frame update 
+     void Start()
+     {
+        durasi = durasiPenilaian;
+
+        soal = assetSoal.ToString().Split('#');
+
+        soalSelesai = new bool[soal.Length];
+
+        soalBag = new string[soal.Length, 6];
+        maxSoal = soal.Length;
+        OlahSoal();
+
+        ambilsoal = true;
+        TampilkanSoal();
+
+        print(soalBag[1,2]);
+     }
+
+      private void OlahSoal()
+      {
+       for(int i=0; i < soal.Length; i++) 
+       {
+         string[] tempSoal = soal[i].Split('+');
+         for(int j = 0; j < tempSoal.Length; j++)
+         {
+            soalBag[i,j] = tempSoal[j];
+            continue;
+         }
+         continue;
+       }
+
+     }
+     private void TampilkanSoal()
+     {
+      if (indexSoal < maxSoal)
+      {
+         if (ambilsoal)
+         {
+            for(int i=0; i < soal.Length; i++)
+            {
+               int randomIndexSoal = Random.Range(0, soal.Length);
+               if(!soalSelesai[randomIndexSoal])
+               {
+            txtSoal.text = soalBag[randomIndexSoal, 0];
+            txtOpsiA.text = soalBag[randomIndexSoal, 1];
+            txtOpsiB.text = soalBag[randomIndexSoal, 2];
+            txtOpsiC.text = soalBag[randomIndexSoal, 3];
+            txtOpsiD.text = soalBag[randomIndexSoal, 4];
+            kuncij = soalBag[indexSoal, 5][0];
+
+            soalSelesai[randomIndexSoal] = true;
+
+            ambilsoal = false;
+            break;
+               }
+               else
+               {
+                  continue;
+               }
+            }
+         }
+      }
+     }
+
+     public void Opsi(string opsiHuruf)
+     {
+      CheckJawaban(opsiHuruf[0]);
+
+      if (indexSoal == maxSoal -1)
+      {
+         isHasil = true;
+      }
+      else
+      {
+         panel.SetActive(true);
+      }
+
+            indexSoal++;
+            ambilsoal = true;
+     }
+     
+     private float HitungNilai()
+     {
+         return nilai = (float)jwbBenar / maxSoal * 100;
+     }
+
+     public Text txtPenilaian;
+     private void CheckJawaban(char huruf)
+     {
+      string penilaian;
+
+      if (huruf.Equals(kuncij))
+      {
+         penilaian = "Benar!";
+         jwbBenar++;
+      }
+      else
+      {
+        penilaian = "Salah!";
+        jwbSalah++;
+      }
+      txtPenilaian.text = penilaian;
+
+     }
+
+     //Update is called once per frame
+     void Update()
+     {
+         if(panel.activeSelf)
+         {
+            durasiPenilaian -=Time.deltaTime;
+
+
+               if (isHasil)
+               {
+                  imgPenilaian.SetActive(true);
+                  imgHasil.SetActive(false);
+
+                  if (durasiPenilaian <= 0)
+                  {
+                     txtHasil.text = "Jumlah benar: "+jwbBenar+"\nJumlah Salah: "+ jwbSalah+"\n\nNilai: "+HitungNilai();
+
+                     imgPenilaian.SetActive(false);
+                     imgHasil.SetActive(true);
+
+                     durasiPenilaian = 0;
+                  }
+               }
+               else
+               {
+                  imgPenilaian.SetActive(true);
+                  imgHasil.SetActive(false);
+
+                  if(durasiPenilaian <= 0) 
+               {
+               panel.SetActive(false);
+               durasiPenilaian = durasi;
+
+               TampilkanSoal();
+               }
+             }
+          }
+     }
+}
---
+++ Assets/Scripts/PengolahWaktu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; // Diperlukan untuk mengelola transisi ke layar game over
+
+public class PengolahWaktu : MonoBehaviour
+{
+    public float waktuAwal = 60.0f; // Waktu awal dalam detik
+    public Text textWaktu; // Teks untuk menampilkan waktu
+
+    private float waktuSisa; // Menyimpan waktu yang tersisa
+    private bool isGameOver = false; // Flag untuk menunjukkan apakah game over
+
+    void Start()
+    {
+        waktuSisa = waktuAwal;
+    }
+
+    void Update()
+    {
+        if (!isGameOver)
+        {
+            waktuSisa -= Time.deltaTime; // Mengurangkan waktu dengan waktu delta
+
+            // Memastikan waktu tidak kurang dari 0
+            if (waktuSisa <= 0)
+            {
+                waktuSisa = 0;
+                GameOver(); // Panggil fungsi game over saat waktu habis
+            }
+
+            UpdateUI(); // Fungsi untuk memperbarui tampilan waktu
+        }
+    }
+
+    void UpdateUI()
+    {
+        textWaktu.text = "" + Mathf.Ceil(waktuSisa).ToString(); // Menampilkan waktu ke teks
+    }
+
+    void GameOver()
+    {
+        isGameOver = true;
+        Debug.Log("Game Over"); // Tulis pesan game over ke konsol (bisa diganti dengan logika game over yang sesungguhnya)
+
+        // Di sini, Anda dapat menambahkan logika lain untuk menampilkan layar game over, seperti mengaktifkan panel game over atau menu game over.
+        
+        // Contoh: Transisi ke layar game over dengan mengganti "NamaSceneGameOver" dengan nama scene yang sesuai
+       // SceneManager.LoadScene("GAME OVER BOY");
+    }
+}
